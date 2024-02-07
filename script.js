159dfcb75ff0a7661c169eb0f39717e48110c4eb
pablopaul01 <juanpablosalomon@gmail.com>
2024-02-07 01:53:54 -0300

    se sube el repositorio a github

---
+++ script.js
@@ -0,0 +1,124 @@
+let munieco = document.querySelector(".contenedormunieco");
+let contenedor = document.querySelector(".contenedor-parrafo");
+let resultado = document.querySelector(".texto-resultado");
+let cajatexto = document.querySelector(".cajatexto");
+
+
+function encriptar(){
+    if(cajatexto.value === ""){
+        alert("Debe ingresa un texto para encriptar");
+    }
+    else
+    {
+        ocultar();
+        console.log(cajatexto.value);
+        resultado.textContent = encriptarTexto(cajatexto.value);
+    }
+}
+
+function desencriptar(){
+    if(cajatexto.value === ""){
+        alert("Debe ingresa un texto para encriptar");
+    }
+    else
+    {
+        ocultar();
+        resultado.textContent = desencriptarTexto(cajatexto.value);
+    }
+}
+
+
+function ocultar(){
+    munieco.classList.add("ocultar");
+    contenedor.classList.add("ocultar");
+}
+
+function encriptarTexto(mensaje){
+    let texto = mensaje;
+    let textoFinal = "";
+
+    for(let i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai"
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter"
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes"
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober"
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    return textoFinal;
+
+}
+
+function desencriptarTexto(mensaje){
+    let texto = mensaje;
+    let textoFinal = "";
+
+    for(let i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a"
+            i = i+1;
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "e"
+            i = i+4;
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "i"
+            i = i+3;
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "o"
+            i = i+3;
+        }
+        
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "u"
+            i = i+3;
+        }
+        else{
+            textoFinal = textoFinal + texto[i];
+        }
+    }
+    return textoFinal;
+}
+
+const btnCopiar = document.querySelector(".btn-copiar"); 
+
+btnCopiar.addEventListener("click", copiar = () => {
+    let contenido = document.querySelector(".texto-resultado").textContent;
+    if(contenido === ""){
+        alert("No hay contenido para copiar");
+    }
+    else
+    {
+        navigator.clipboard.writeText(contenido); 
+        if (munieco.classList.contains("ocultar") && contenedor.classList.contains("ocultar")) {
+            munieco.classList.remove("ocultar");
+            contenedor.classList.remove("ocultar");
+            resultado.textContent = "";
+            cajatexto.value = "";
+        }
+        alert("Texto copiado");
+    }
+});
+
+cajatexto.addEventListener("input", (e) => {
+    if (cajatexto.value === "" && munieco.classList.contains("ocultar") && contenedor.classList.contains("ocultar")) {
+        munieco.classList.remove("ocultar");
+        contenedor.classList.remove("ocultar");
+        resultado.textContent = "";
+    }
+}
+)
